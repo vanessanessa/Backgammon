cea25f69d5520de9f7c43d2fb978f7c87b7f5f4e
Thommy Berglund <thommy.berglund@gmail.com>
2017-09-29 17:33:52 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>
+Backgammon Scoreboard
+</title>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.11.0/umd/popper.min.js" integrity="sha384-b/U6ypiBEHpOf/4+1nzFpr53nxSS+GLCkfwBdFNTxtclqqenISfwAzpKaMNFNmj4" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/js/bootstrap.min.js" integrity="sha384-h0AbiXch4ZDo7tp9hKZ4TsHbi047NrKGLO3SEJAg45jXxnGIfYzk4Si90RDIqNm1" crossorigin="anonymous"></script>
+<style>
+.table td{
+   text-align: center;   
+}
+.table th{
+   text-align: center;   
+}
+</style>
+</head>
+<body>
+    <div class="text-center">
+        <img class="rounded mx-auto d-block" width = "400" height = "200" src="res/backgammon.jpg"/>
+    </div>
+    <div class="p-3">
+    <table class="table" style="width: 70%;" align="center">
+    <thead class="thead-default">
+      <tr>
+        <th>#</th>
+        <th>Vanessa</th>
+        <th>Thommy</th>
+        
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>0</td>
+        <td>8</td>
+    
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>0</td>
+        <td>2</td>
+
+      </tr>
+      </tbody>
+      <tfoot class="thead-default">
+      <tr>
+        <th>Sum</th>
+        <th>0</td>
+        <th>10</td>
+
+      </tr>
+      </tfoot>
+   
+  </table>
+  </div>
+</body>
+</html>
